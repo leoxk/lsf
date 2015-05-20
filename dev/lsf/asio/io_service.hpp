4c0ebadf605ea97d022145fd7bff2ca6f6afc15f
rangerheart <leoxiang727@qq.com>
2015-05-20 14:30:03 +0800

    asio modify

---
+++ dev/lsf/asio/io_service.hpp
@@ -0,0 +1,23 @@
+// File:        epoll_service.hpp
+// Description: ---
+// Notes:       ---
+// Author:      leoxiang <leoxiang727@qq.com>
+// Revision:    2015-05-19 by leoxiang
+
+#include "lsf/basic/singleton.hpp"
+#include "lsf/asio/async/basic_proactor.hpp"
+#include "lsf/asio/async/epoll_event.hpp"
+#include "lsf/asio/async/poll_event.hpp"
+
+namespace lsf {
+namespace asio {
+
+typedef async::BasicProactorSerivce<async::EpollEvent> EpollIOService;
+
+typedef async::BasicProactorSerivce<async::PollEvent>  PollIOService;
+
+
+} // end of namespace asio
+} // end of namespace lsf
+
+// vim:ts=4:sw=4:et:
---
+++ dev/test/asio/test_asio.cpp
@@ -0,0 +1,51 @@
+// File:        test_asio.cpp
+// Description: ---
+// Notes:       ---
+// Author:      leoxiang <leoxiang727@qq.com>
+// Revision:    2015-05-19 by leoxiang
+
+#include "lsf/basic/unit_test.hpp"
+#include "lsf/asio/tcp.hpp"
+#include "lsf/asio/io_service.hpp"
+#include "lsf/util/random.hpp"
+
+using namespace std;
+using namespace lsf::asio;
+using namespace lsf::basic;
+using namespace lsf::util;
+
+bool AcceptFunc()
+{
+    std::cout << "get a accept" << std::endl;
+    return true;
+}
+
+LSF_TEST_CASE(test_asio)
+{
+    uint32_t listen_port = SingleRandom::Instance()->GetRand(15000, 16000);
+
+    // listen
+    tcp::ListenSocket listen_socket;
+    LSF_ASSERT(listen_socket.Bind(tcp::SockAddr(ip::Address::Any(), listen_port)));
+    LSF_ASSERT(listen_socket.Listen());
+
+    // connect
+    tcp::Socket socket;
+    LSF_ASSERT(socket.Connect(tcp::SockAddr(ip::Address::Any(), listen_port)));
+
+    // async accept
+    EpollIOService io_service;
+    LSF_ASSERT(io_service.AsyncAccept(listen_socket, std::bind(AcceptFunc)));
+    std::cout << io_service.ErrString() << std::endl;
+
+    io_service.Run();
+}
+
+int main(int argc, char **argv)
+{
+	LSF_TEST_ALL();
+}
+
+
+
+// vim:ts=4:sw=4:et:
